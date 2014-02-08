db05a815f964c232f4ed3f99e22c8a9c305a4a4d
Nadezhda Shivarova <nadia93.ie@gmail.com>
2014-02-08 17:13:26 +0000

    Pass pressure value to SD function from sensor

---
+++ SD_main/SD_main.ino
@@ -0,0 +1,60 @@
+//test if microSD has been inserted and upload the data from
+//sensor into .csv file with timestamp on the SD
+//using the predefined functions
+#include <SD.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP085_U.h>
+
+//instance of the sensor
+Adafruit_BMP085_Unified sensor = Adafruit_BMP085_Unified(10085);
+
+//initialised variable to save pressure value
+double pressure = 0;
+
+void setup()
+{
+  SD_setup();
+
+
+
+  /* Initialise the sensor */
+  if(!sensor.begin())
+  {
+    /* There was a problem detecting the BMP085 ... check your connections */
+    Serial.print("Ooops, no BMP085 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+
+}
+void loop() 
+{
+  
+  
+  sensors_event_t event;
+  sensor.getEvent(&event);
+  
+    if (event.pressure)
+  {
+    /* Display atmospheric pressue in hPa */
+    Serial.print("Pressure:    ");
+    Serial.print(event.pressure);
+    Serial.println(" hPa");
+    pressure = event.pressure;
+  }
+
+  else
+  {
+    Serial.println("Sensor error");
+  }
+  
+  SD_write(pressure);
+  
+  //10 seconds delay between readings
+  delay(5000);
+  
+  
+  
+
+}
+
---
+++ SD_main/SD_write.ino
@@ -0,0 +1,8 @@
+//function to write each sensor reading to the file in the SD card
+//if you wish to modify object, leave out 'const'
+void SD_write(float p)
+{
+  Serial.println(p);
+  Serial.println("^passed");
+  
+}
